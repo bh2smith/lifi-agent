419db2f7f0636be5608c91cf3548549c6bf77423
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-06-26 11:54:32 +0200

    Manifest Simplification

--- src/app/api/ai-plugin/route.ts
+++ src/app/api/ai-plugin/route.ts
@@ -1,34 +1,14 @@
 import { NextResponse } from "next/server";
 import { ACCOUNT_ID, PLUGIN_URL } from "../../config";
-
-// sharedParameters.ts
-const chainIdParam = {
-  name: "chainId",
-  in: "query",
-  required: true,
-  description: "EVM Network (aka chain ID)",
-  schema: { type: "number" },
-  example: 100,
-};
-
-const addressOrSymbolParam = {
-  name: "address",
-  in: "query",
-  required: true,
-  description:
-    "The ERC-20 token symbol or address to be sold, if provided with the symbol do not try to infer the address.",
-  schema: { type: "string" },
-  example: "0x6810e776880c02933d47db1b9fc05908e5386b96",
-};
-
-const addressParam = {
-  name: "address",
-  in: "query",
-  required: true,
-  description: "20 byte Ethereum address with 0x prefix",
-  schema: { type: "string" },
-};
-
+import {
+  chainIdParam,
+  addressParam,
+  addressOrSymbolParam,
+  SignRequestResponse200,
+  SignRequestSchema,
+  AddressSchema,
+  MetaTransactionSchema,
+} from "@bitte-ai/agent-sdk";
 export async function GET() {
   const pluginData = {
     openapi: "3.0.0",
@@ -107,130 +87,12 @@ export async function GET() {
         evmAddress: { ...addressOrSymbolParam, name: "evmAddress" },
       },
       responses: {
-        SignRequestResponse200: {
-          description:
-            "Transaction Payload along with some additional metadata related to the transaction bytes.",
-          content: {
-            "application/json": {
-              schema: {
-                type: "object",
-                properties: {
-                  transaction: {
-                    $ref: "#/components/schemas/SignRequest",
-                  },
-                  meta: {
-                    type: "object",
-                    description:
-                      "Additional metadata related to the transaction",
-                    additionalProperties: true,
-                    example: {
-                      message: "Order submitted successfully",
-                    },
-                  },
-                },
-                required: ["transaction"],
-              },
-            },
-          },
-        },
+        SignRequestResponse200,
       },
       schemas: {
-        Address: {
-          description:
-            "20 byte Ethereum address encoded as a hex with `0x` prefix.",
-          type: "string",
-          example: "0x6810e776880c02933d47db1b9fc05908e5386b96",
-        },
-        MetaTransaction: {
-          description: "Sufficient data representing an EVM transaction",
-          type: "object",
-          properties: {
-            to: {
-              $ref: "#/components/schemas/Address",
-              description: "Recipient address",
-            },
-            data: {
-              type: "string",
-              description: "Transaction calldata",
-              example: "0xd0e30db0",
-            },
-            value: {
-              type: "string",
-              description: "Transaction value",
-              example: "0x1b4fbd92b5f8000",
-            },
-          },
-          required: ["to", "data", "value"],
-        },
-        SignRequest: {
-          type: "object",
-          required: ["method", "chainId", "params"],
-          properties: {
-            method: {
-              type: "string",
-              enum: [
-                "eth_sign",
-                "personal_sign",
-                "eth_sendTransaction",
-                "eth_signTypedData",
-                "eth_signTypedData_v4",
-              ],
-              description: "The signing method to be used.",
-              example: "eth_sendTransaction",
-            },
-            chainId: {
-              type: "integer",
-              description:
-                "The ID of the Ethereum chain where the transaction or signing is taking place.",
-              example: 100,
-            },
-            params: {
-              oneOf: [
-                {
-                  type: "array",
-                  items: {
-                    $ref: "#/components/schemas/MetaTransaction",
-                  },
-                  description: "An array of Ethereum transaction parameters.",
-                },
-                {
-                  type: "array",
-                  items: {
-                    type: "string",
-                  },
-                  description: "Parameters for personal_sign request",
-                  example: [
-                    "0x4578616d706c65206d657373616765",
-                    "0x0000000000000000000000000000000000000001",
-                  ],
-                },
-                {
-                  type: "array",
-                  items: {
-                    type: "string",
-                  },
-                  description: "Parameters for eth_sign request",
-                  example: [
-                    "0x0000000000000000000000000000000000000001",
-                    "0x4578616d706c65206d657373616765",
-                  ],
-                },
-                {
-                  type: "array",
-                  items: {
-                    type: "string",
-                  },
-                  description:
-                    "Parameters for signing structured data (TypedDataParams)",
-                  example: [
-                    "0x0000000000000000000000000000000000000001",
-                    '{"data": {"types": {"EIP712Domain": [{"name": "name","type": "string"}]}}}',
-                  ],
-                },
-              ],
-            },
-          },
-        },
+        Address: AddressSchema,
+        MetaTransaction: MetaTransactionSchema,
+        SignRequest: SignRequestSchema,
       },
     },
   };
